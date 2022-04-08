6e5cabe66f5c64e65630624ede9e1d1e79371ed8
felipeb239 <felipeb239@gmail.com>
2022-04-08 16:22:19 -0300

    First commit intellij

---
+++ src/flp/brg/ProximePrograms.java
@@ -0,0 +1,18 @@
+package flp.brg;
+
+import flp.brg.model.Gato;
+
+public class ProximePrograms {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+
+        /* int a = 5;
+           int b = 3;
+        System.out.println("TESTE JULIUS " + (a+b)); */
+
+
+    }
+}
